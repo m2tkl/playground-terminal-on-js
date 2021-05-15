3697db8c040b2a1a2f5d67d8ec967664f1e546bb
m2tkl <kenchalros@gmail.com>
2021-05-15 20:22:24 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,103 @@
+const main = {
+  data() {
+    return {
+      cmd: '',
+      logs: [],
+      workingDir: '~',
+      files: {
+        type: 'dir',
+        name: '~',
+        items: [
+          {
+            type: 'dir',
+            name: 'hoge',
+            items: [
+              { type: 'file', name: 'hoge1.txt', contents: 'hello hoge1.' },
+              { type: 'file', name: 'hoge2.txt', contents: 'hello hoge2.' },
+            ]
+          },
+          {
+            type: 'dir',
+            name: 'fuga',
+            items: [
+              { type: 'file', name: 'fuga1.txt', contents: 'hello hoge1.' },
+              { type: 'file', name: 'fuga2.txt', contents: 'hello hoge2.' },
+              { type: 'dir', name: 'fugafuga', items: [] }
+            ]
+          },
+          { type: 'file', name: 'piyo.txt', contents: 'piyopiyo\nthis is a piyo.' },
+        ],
+      }
+    }
+  },
+  methods: {
+    exec: function() {
+      const cmd = this.cmd.split(/\s+/)
+      console.log(cmd)
+      this.logs.push(this.workingDir)
+      this.logs.push('$ ' + this.cmd)
+      if (!cmd[0]) {
+        // pass
+      } else if (cmd[0] === 'clear') {
+        this.clearHistory()
+        return
+      } else if (cmd[0] === 'ls') {
+        items = this.searchDir(this.files, this.workingDir)
+        names = this.getNameOfItems(items)
+        this.logs.push(names.join('\t'))
+      } else if (cmd[0] === 'cd') {
+        console.log(this.workingItems)
+        for (let item of this.workingItems) {
+          if (item.name === cmd[1] && item.type === 'dir') {
+            console.log(item.name)
+          }
+        }
+      } else if (cmd[0] === 'cat') {
+        for (let item of this.workingItems) {
+          if (item.name === cmd[1] && item.type === 'file') {
+            this.logs.push(item.contents);
+          }
+        }
+      } else {
+        this.logs.push('command not found: ' + cmd)
+      }
+      this.logs.push(' ')
+      this.cmd = ''
+    },
+    clearHistory: function() {
+      this.logs = []
+      this.cmd = ''
+    },
+    searchDir: function(files, name) {
+      if (files.type === 'dir') {
+        if (files.name === name) {
+          return files.items
+        }
+        console.log(files.name)
+        for (let item of files.items) {
+          this.searchDir(item)
+        }
+      }
+    },
+    getNameOfItems: function(items) {
+      names = []
+      for (let item of items) {
+        if (item.type === 'dir') {
+          names.push(item.name + '/')
+        } else {
+          names.push(item.name)
+        }
+      }
+      return names
+    }
+  },
+  computed: {
+    workingItems() {
+      items = this.searchDir(this.files, this.workingDir)
+      return items
+    },
+  }
+}
+
+const app = Vue.createApp(main)
+app.mount('#app')
