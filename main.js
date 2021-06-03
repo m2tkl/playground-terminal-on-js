f3658a83c7a99c13e64bf63287b4e1f63267c3ee
m2tkl <kenchalros@gmail.com>
2021-06-04 01:10:23 +0900

    typingコンポーネントに渡すデータを追加

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const main = {
   data() {
     return {
+      introduction: `Hi, I'm Piyopiyo.
+This is my introduction page.
+
+Please run 'help' command to know how to explore this page.
+
+Have fun!
+(You can input after '$')`
     }
   },
-  methods: {
-  }
+  mounted() {
+    console.log('み〜た〜な〜')
+  },
 }
 
 const app = Vue.createApp(main)
