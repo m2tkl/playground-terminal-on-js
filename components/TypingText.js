25526f50f34a20e2beef6a99ebb17ebf9369e0c0
m2tkl <kenchalros@gmail.com>
2021-06-04 01:09:02 +0900

    タイピングしているようなテキスト表示を行うコンポーネントを追加

---
+++ components/TypingText.js
@@ -0,0 +1,46 @@
+app.component('typing-text', {
+  template:
+  /* html */
+  `
+  <div :class="isTyping">
+    <code>
+      <p>{{ typedText }}</p>
+    </code>
+  </div>
+  `,
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    speed: {
+      type: Number, 
+      required: false,
+      default: 100
+    }
+  },
+  data() {
+    return {
+      typedText: '',
+      timer: null,
+      count: 0,
+      isTyping: "typing"
+    }
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      this.typedText = this.text.substr(0, this.count);
+      if (this.count > this.typedText.length) {
+        this.isTyping = ''
+        clearInterval(this.timer)
+      }
+      this.count++;
+    }, this.speed);
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+})
